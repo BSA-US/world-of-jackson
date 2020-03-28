055a7d867348bd4d35fb63af96a44d62cb12467a
MarsKid <marskid@MarsKids-MacBook-Pro.local>
2020-03-28 17:15:32 -0400

    map in progress

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -34,6 +34,52 @@ export function Map ({ callbackRegistration, callback }: MapProps) {
 
         })
 
+        localMap.once('style.load', () => {
+
+
+            localMap.addLayer(
+            {
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+                    
+                    // use an 'interpolate' expression to add a smooth transition effect to the
+                    // buildings as the user zooms in
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'height']
+                    ],
+                    'fill-extrusion-base': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'min_height']
+                    ],
+                    'fill-extrusion-opacity': 0.6
+                }
+            });
+            
+            localMap.on('click', '3d-buildings', function(e) {
+                new MapBox.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(e.features[0].properties.name )
+                .addTo(localMap);
+                });
+        })
+
         /*
         var layers: any = localMap.getStyle().layers;
         var labelLayerId;
