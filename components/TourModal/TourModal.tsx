f21e556cd58e455ba4354c80c7f32b1d1ea1458c
Mike Kissinger <mis4mike@gmail.com>
2020-07-03 13:39:07 -0400

    fix missed merge conflict

--- components/TourModal/TourModal.tsx
+++ components/TourModal/TourModal.tsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-import type { FunctionComponent } from 'react'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS } from '@contentful/rich-text-types';
-import { ITourNode } from '../TourBar/TourBar';
-
-import UITheme from 'styled-components';
-// var sanitizeHtml: any = require('sanitize-html');
-=======
 import React from "react";
 import type { FunctionComponent } from "react";
-
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
 import { ITourNode } from "../TourBar/TourBar";
 
 import UITheme from "styled-components";
-var sanitizeHtml: any = require("sanitize-html");
->>>>>>> socs(storybook): BSA-US/world-of-jackson#47 Write typography styles and create text preview page
+// var sanitizeHtml: any = require('sanitize-html');
 
 const InfoArea = UITheme.div`
     background-color: #0f1007;
@@ -31,31 +21,34 @@ const InfoArea = UITheme.div`
     left: 50%;
     top: 80px;
     margin: 0 -200px 0 0;
-`
+`;
 
 const EmbeddedImage = UITheme.img`
   width: 100%;
-`
+`;
 
-const TourModal: FunctionComponent<{ selectedTourNode: ITourNode | null }> =
-  ({ selectedTourNode }) => {
+const TourModal: FunctionComponent<{ selectedTourNode: ITourNode | null }> = ({
+  selectedTourNode,
+}) => {
+  const options: any = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (input: any) => {
+        const fields: any = input.data.target.fields;
+        /// alternative rendering:
+        //return <img src={ fields.file.url} height={fields.file.details.image.height} width={fields.file.details.image.width} alt={fields.description} />;
+        return <EmbeddedImage src={fields.file.url} alt={fields.description} />;
+      },
+    },
+  };
+  let description = selectedTourNode
+    ? documentToReactComponents(selectedTourNode.description, options)
+    : null;
+  return (
+    <InfoArea>
+      <h3>{selectedTourNode?.label}</h3>
+      <p>{description}</p>
+    </InfoArea>
+  );
+};
 
-    const options: any = {
-      renderNode: {
-          [BLOCKS.EMBEDDED_ASSET]: (input: any) => {
-            const fields: any = input.data.target.fields
-            /// alternative rendering:
-            //return <img src={ fields.file.url} height={fields.file.details.image.height} width={fields.file.details.image.width} alt={fields.description} />;
-            return <EmbeddedImage src={ fields.file.url} alt={fields.description} />;
-      }},
-    };
-    let description = selectedTourNode ? documentToReactComponents(selectedTourNode.description, options) : null;
-      return (
-        <InfoArea>
-          <h3>{selectedTourNode?.label}</h3>
-          <p>{description}</p>
-        </InfoArea>
-      )
-  }
-
-export default TourModal
+export default TourModal;
