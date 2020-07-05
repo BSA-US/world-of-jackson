b1f557be09165ab3deee22af7bf1d2ed02125183
Tyler Freeman <git@odbol.com>
2020-07-04 17:56:12 -0700

    Remove d3

--- components/Map/ConnectionArcsLayer.ts
+++ components/Map/ConnectionArcsLayer.ts
@@ -1,46 +1,7 @@
 import { ArcLayer } from "@deck.gl/layers";
 
-import { scaleLinear } from "d3-scale";
-
 // import { Entry } from 'contentful';
 import { IBuilding } from '~/types/db/contentful';
-
-//@ts-ignore
-// export const COLOR_SCALE = scaleThreshold()
-// .domain([
-//   -0.6,
-//   -0.45,
-//   -0.3,
-//   -0.15,
-//   0,
-//   0.15,
-//   0.3,
-//   0.45,
-//   0.6,
-//   0.75,
-//   0.9,
-//   1.05,
-//   1.2
-// ])
-// .range([
-//   [65, 182, 196],
-//   [127, 205, 187],
-//   [199, 233, 180],
-//   [237, 248, 177],
-//   // zero
-//   [255, 255, 204],
-//   [255, 237, 160],
-//   [254, 217, 118],
-//   [254, 178, 76],
-//   [253, 141, 60],
-//   [252, 78, 42],
-//   [227, 26, 28],
-//   [189, 0, 38],
-//   [128, 0, 38]
-// ]);
-
-//@ts-ignore
-const COLOR_SCALE = scaleLinear().domain([0, 100]).range([[255, 255, 204], [128, 0, 38]]);
 
 function getCenterCoords(feature: IBuilding): ILatLon {
   // TODO(odbol): fix latitutde spelling???
@@ -66,10 +27,10 @@ function createFakeConnectionsData(objects: { [key: string]: any }): IConnection
     connections.push({
       start: getCenterCoords(features[i]),
       end: getCenterCoords(endFeature),
-      startColor: COLOR_SCALE(0),
-      endColor: COLOR_SCALE(100)
+      // TODO(odbol): use the color of the buildings? or custom color depending on connection type??
+      startColor: [255, 180, 204],
+      endColor: [178, 0, 38]
     });
-    console.log(`createFakeConnectionsData: ${connections[i].startColor} == ${COLOR_SCALE(0)}, ${connections[i].endColor} == ${COLOR_SCALE(100)}, ${COLOR_SCALE(Math.random() * 100)}`);
   }
   return connections;
 }
